c3014f94038083d6a599a16ad1fa7468ab97e3f3
highestpeak <highestpeak@163.com>
2020-06-16 23:25:42 +0800

    演示

--- fly_advice_ui/src/main.js
+++ fly_advice_ui/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import 'font-awesome/css/font-awesome.css'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
